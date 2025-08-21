fd3bc486daf867bc77f93cce71769247bbdb6310
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-21 13:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
+	const rows = prompt("Input number of rows")
+	const cols = prompt("Input number of columns")
+	const table = document.getElementById("myTable")
+	for(let i = 0;i<rows;i++){
+		let Roww = document.createElement("tr")
+			for(let j = 0;j<cols;j++){
+				let Coll = document.createElement("td")
+				Coll.innerText = `Row-${i} Column-${j}`
+				console.log(Coll.innnerText)
+				Roww.appendChild(Coll)
+			}
+				table.appendChild(Roww)
+		}
+	
   
 }
